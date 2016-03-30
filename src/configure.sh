6e07d679b39254d98aba33a11ec2373cfadcd5cb
Max <you@example.com>
2016-03-30 13:21:23 +0200

    changed configure

--- src/configure.sh
+++ src/configure.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/bash
 
+if [$1 = "-m"];then
+    echo"Module loading enabled!"
+    module purge
+    module load gcc/4.9.2
+    module load cuda/7.5
+    module load hdf5
+    module load cmake/3.3.0
+    module load boost
+fi
+
+
 cmake .
 mv CMakeFiles/ build/
 mv CMakeCache.txt build/
