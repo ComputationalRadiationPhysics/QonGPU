6aa76f3d6d6c60d05f97f5963640044d1682a637
Max <you@example.com>
2016-04-06 11:28:09 +0200

    Made backwards iteration working

--- src/SchroedingerSolver/include/static/Numerov1D.hpp
+++ src/SchroedingerSolver/include/static/Numerov1D.hpp
@@ -18,12 +18,12 @@
 
 #define DEBUG(x) std::cout<<x<<endl;
 
-#define FORWARDS
+#define BACKWARDS
 
 __device__  double V(double x, double t,double z) {
-  return z/sqrt(1+x*x);
+  return z/(1+x*x);
 };
-
+//NumerovKernel to iterate from right to left!
 __global__ void NumerovKernel2(double* psi,
                                int nx,
                                int ne,
@@ -33,18 +33,22 @@ __global__ void NumerovKernel2(double* psi,
     //Should always show to the psi_n(x=0) at the energy level E_n
     int tid = nx * ( threadIdx.x + blockIdx.x * blockDim.x );
     int offset = nx * blockDim.x * gridDim.x;
-    double dx = fabs( xmax - xmin ) / ( (double) nx );
-    double E = -1.0;
-    double dE = E / ( (double) ne );
+    double dx = ( xmax - xmin ) / nx;
+    double E = -V(0,0,z);
+    double dE = E/ne;
     double f1, f2,f3;
-    double heff = 1 / 12 * dx * dx;
+    double heff = dx * dx / 12;
     while(tid < nx * ne) {
-        E = dE * (double) tid / (double) nx;
-        printf("Using the energy: %lf \n",E);
-        for (auto i = nx - 1; i >= 2; i -= 1) {
-            f1 = 1.0 + heff * (E + V(xmax - (i - 2) * dx , 0, z));
-            f2 = 1.0 - 5.0 * heff * (E + V(xmax -( i - 1) * dx, 0, z));
-            psi[i - 2 + tid] = (2 * psi[i - 1 + tid] * f2 - psi[i  + tid] * f3) / f1;
+        E = dE * tid /  nx;
+        for(auto i = nx-1; i>1;i--) {
+            //refine the first pre-factor for psi_(n-1)
+            f1 = 1.0 + heff * (E + V(xmax - (i-2) * dx, 0, z));
+            //redefine the next prefactor for psi_n
+            f2 = 1.0 - 5.0 * heff * (E + V(xmax - (i-1) *dx, 0, z));
+            //redefine at lat the third prefactor for psi_(n+1)
+            f3 = 1.0 + heff * (E + V(xmax - i * dx , 0, z));
+            //finally let's calculate the new value psi_n-1=1/f1 * (2 * psi_n * f2 - psi_n+1 * f3)
+            psi[ i + tid - 2 ] = 1 / f1 * (2 * psi[i+tid-1] * f2 - psi[ i+ tid ] * f3);
         }
         tid += offset;
     }
@@ -56,20 +60,20 @@ __global__ void NumerovKernel(double* psi,
                               double xmax,
                               double xmin,
                               double z) {
-	unsigned int tid = nx*(threadIdx.x+blockIdx.x*blockDim.x);//Should always show to the psi_n(x=0) at the energy level E_n
-    uint offset = nx*blockDim.x*gridDim.x;
+	int tid = nx*(threadIdx.x+blockIdx.x*blockDim.x);//Should always show to the psi_n(x=0) at the energy level E_n
+    int offset = nx*blockDim.x*gridDim.x;
 	double dx = fabs(xmax-xmin)/((double)nx);
-	double E = -V(0,0,z);
+	double E = V(0,0,z);
 	double dE=E/((double)ne);
 	double f1,f2,f3;
 	double heff=1.0;
 	while(tid<ne*nx){
 		E=tid*dE/(nx);
 		for(auto i = 2; i<nx ;i++){
-			f1=1/(1+dx*dx*(heff*(E+V((i+1)*dx+xmin,0,z)))/12);
-			f2=(1-5*dx*dx/12*(heff*(E+V(i*dx+xmin,0,z))));
-			f3=(1+dx*dx/12*(heff*(E+V((i-1)*dx+xmin,0,z))));	
-			psi[i+1+tid]=f1*(2*psi[i+tid]*f2-psi[i-1+tid]*f3);
+			f1=1.0/(1.0+dx*dx*(heff*(V((i+1)*dx+xmin,0,z)-E))/12.0);
+			f2=(1.0-5.0*dx*dx/12.0*(heff*(V(i*dx+xmin,0,z)-E)));
+			f3=(1.0+dx*dx/12.0*(heff*(V((i-1)*dx+xmin,0,z)-E)));
+			psi[i+1+tid]=f1*(2.0*psi[i+tid]*f2-psi[i-1+tid]*f3);
 		};
 		tid+=offset;
     };
@@ -82,14 +86,15 @@ public:
     void solve();
     void allocresult();
     void bisect();
-    Numerov1D(Params1D* pa,std::complex<double>* ps):param(pa),p(ps)
-    ,pot(),eindex(100),out(pa){
+    void bisect2();
+    Numerov1D(Params1D* pa,std::complex<double>* ps) : param(pa), p(ps)
+    , pot(), eindex(100), out(pa) {
 	nx = (param->getnx());
 	ne =  (param->getne());
 	psi=(double*) malloc(sizeof(double)*nx*ne);
 #ifdef BACKWARDS
 	if(psi!=NULL){
-	    for(size_t i = nx; i < nx*ne; i+=nx){
+	    for(size_t i = nx-1; i < nx*ne; i+=nx){
 		    psi[i]=0;
 		    psi[i-1]=-1e-10;
 	    };
@@ -103,10 +108,11 @@ public:
 	    };
 	}
 #endif
-	else{
-	    std::cout<<"Error: Allocation failed!"<<std::endl;
-	    assert(psi!=NULL);
-	}
+    else{
+	        std::cout<<"Error: Allocation failed!"<<std::endl;
+            std::cout<<"Please make sure to have at least: "<<nx*ne*4/1024/1024<<"megabytes RAM availible!"<<endl;
+            assert(psi!=NULL);
+	    }
     };
     Numerov1D(){};
     ~Numerov1D(){
@@ -120,12 +126,13 @@ protected:
     double* psi;
     Params1D* param;
     Potential1D pot;
-    void tempprint(double* temp,Params1D* p);
     size_t nx,ne;
     bool sign(double x);
     int hindex=0;
     double z=param->getz();
     IOHandle1D out;
+    void tempprint(double* temp,Params1D* p);
+
 };
 
 
@@ -136,18 +143,18 @@ protected:
  */ 
 void Numerov1D::solve(){
     double* dev_ptr;
-    //Allocate needed Memomry, in this case an nx*ne double array!
+    //Allocate required device Memomry, in this case an nx*ne double array!
     cudaMalloc((void**)&dev_ptr,sizeof(double)*nx*ne);
     //copy Memory to the device
     cudaMemcpy(dev_ptr,psi,sizeof(double)*nx*ne,cudaMemcpyHostToDevice);
     //call the actual Kernel
 #ifdef BACKWARDS
-    DEBUG("Running backwards iteration")
-    NumerovKernel2<<<256,3>>>(dev_ptr,nx,ne,param->getxmax(),param->getxmin(),z);
+    DEBUG("Running backwards iteration!")
+    NumerovKernel2<<<256,4>>>(dev_ptr,nx,ne,param->getxmax(),param->getxmin(),z);
 #endif
 #ifdef FORWARDS
-    DEBUG("Running forwards iteration")
-    NumerovKernel<<<256,3>>>(dev_ptr,nx,ne,param->getxmax(),param->getxmin(),z);
+    DEBUG("Running forwards iteration!")
+    NumerovKernel<<<256,4>>>(dev_ptr,nx,ne,param->getxmax(),param->getxmin(),z);
 #endif
     //fetch the results into the psi array!
     cudaMemcpy(psi,dev_ptr,sizeof(double)*nx*ne,cudaMemcpyDeviceToHost);
@@ -156,6 +163,9 @@ void Numerov1D::solve(){
     //use bisection method
 #ifdef  FORWARDS
     bisect();
+#endif
+#ifdef BACKWARDS
+    bisect2();
 #endif
     //generate output
     tempprint(psi,param);
@@ -236,5 +246,32 @@ void Numerov1D::bisect(){
 	DEBUG("Finished!")
 	DEBUG(hindex<<" Energy levels were found!")
 };
+
+void Numerov1D::bisect2() {
+    auto act = false;
+    auto prev = false;
+    double fine = 1;
+    hindex=0;
+    for(auto i = 1;i < ne; i++){
+        act = sign(psi[nx*i]);
+        prev = sign(psi[nx*(i-1)]);
+        if(!(act==prev)) {
+            if (fabs(psi[nx * i]) <= fine) {
+                DEBUG(psi[nx * i])
+                eindex[hindex] = i - 1;
+                hindex += 1;
+            }
+
+            else {
+                DEBUG("Psi was too large")
+            }
+        }
+    DEBUG("Current sign: "<<act<<" Sign before"<<prev)
+    }
+
+    DEBUG("----------------------------------")
+    DEBUG("Finished!")
+    DEBUG(hindex<<" Energy levels were found!")
+}
 
 #endif
