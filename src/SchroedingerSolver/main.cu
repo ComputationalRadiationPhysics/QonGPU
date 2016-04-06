d7e7541c3bb3dd4475a0c22d5bbefd8234c4e0e2
Max <you@example.com>
2016-04-06 09:28:08 +0200

    changed some parameters

--- src/SchroedingerSolver/main.cu
+++ src/SchroedingerSolver/main.cu
@@ -7,11 +7,11 @@
 #include "include/SimDef.hpp"
 
 int main(int argc, char** argv) {
-  std::complex<double> xma = 15.0;
-  std::complex<double> xmi = -15.0;
+  std::complex<double> xma = 50.0;
+  std::complex<double> xmi = -50.0;
   std::complex<double> tmi = 0.0;
   std::complex<double> tma = 5.0;
-  Params1D p(xma, xmi, tma, tmi, 1e5, 2, 1000,1);
+  Params1D p(xma, xmi, tma, tmi, 1e7, 2, 10,1);
   SimDef<Numerov1D, TimeOperator1D, IOHandle, Core1D, Params1D, 1> s(&p);
   s.staticsolve();
   return EXIT_SUCCESS;
