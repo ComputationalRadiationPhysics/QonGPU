f278e6fc03a7df8833a5380885c64adc64ecc0c2
Max <you@example.com>
2016-04-11 18:52:09 +0200

    Some refactoring done and in-source build disabled

---
+++ configure.sh
@@ -0,0 +1,36 @@
+
+#!/bin/sh
+
+mkdir build/
+
+cd build
+cmake .. || exit 1
+
+
+
+
+#check number of cores for later use with make
+cores=1
+if [ $(which nproc) ]
+then
+	cores=$(nproc)
+elif [ $(which grep) -a -f /proc/cpuinfo ]
+then
+	cores=$(grep -c ^processor /proc/cpuinfo)
+fi
+
+echo "Found number of Cores: ${cores}. Use $(expr ${cores} + 1) jobs for parallel build using make."
+
+echo "Running make"
+
+make -j$((cores+1)) --no-print-directory || exit 1
+
+echo "Finished"
+
+echo "Running qsolve"
+build/Solver/qsolve
+
+#if [ "$1"=="-py" ]; then
+#        echo "Use Python graphics script"
+#        python pyscripts/Graphicscript.py
+#fi
